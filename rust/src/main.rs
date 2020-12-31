0bfdd526473b6ffa8e11c340344e8f3b531a3581
Octave Larose <octave.larose@hotmail.fr>
2020-12-31 11:51:23 +0100

    Fixed some possible map size bugs + split into functions

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -4,37 +4,33 @@ use sdl2::pixels::Color;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use std::time::Duration;
+use sdl2::rect::Rect;
 
+const MAP_X: u32 = 50;
+const MAP_Y: u32 = 50;
+const MAP_SIZE: u32 = MAP_X * MAP_Y;
+const MAP_TILE_SIZE: u32 = 10;
 
-pub fn main() {
-    let sdl_context = sdl2::init().unwrap();
-    let video_subsystem = sdl_context.video().unwrap();
-    const MAP_X: u32 = 70;
-    const MAP_Y: u32 = 70;
-    const MAP_SIZE: u32 = MAP_X * MAP_Y;
-    const MAP_TILE_SIZE: u32 = 10;
+fn init_map() -> [Rect; MAP_SIZE as usize] {
     let mut map: [sdl2::rect::Rect; MAP_SIZE as usize] = [sdl2::rect::Rect::new(0, 0, 0, 0); MAP_SIZE as usize];
-
-    let window = video_subsystem.window("rust-sdl2 demo", MAP_X * MAP_TILE_SIZE, MAP_Y * MAP_TILE_SIZE)
-        .position_centered()
-        .build()
-        .unwrap();
-
-    let mut canvas = window.into_canvas().build().unwrap();
 
     for i in 0..MAP_SIZE {
         map[i as usize] = sdl2::rect::Rect::new((i % MAP_X * MAP_TILE_SIZE) as i32,
-                                       (i / MAP_Y * MAP_TILE_SIZE) as i32,
-                                       MAP_TILE_SIZE,
-                                       MAP_TILE_SIZE);
-     }
+                                                (i / MAP_X * MAP_TILE_SIZE) as i32,
+                                                MAP_TILE_SIZE,
+                                                MAP_TILE_SIZE);
+    }
 
-    canvas.set_draw_color(Color::RGB(0, 255, 255));
-    canvas.clear();
-    canvas.present();
+    map
+}
+
+fn game_loop(sdl_context: sdl2::Sdl,
+             mut canvas: sdl2::render::Canvas<sdl2::video::Window>,
+             map: [Rect; MAP_SIZE as usize]) {
     let mut event_pump = sdl_context.event_pump().unwrap();
     let mut i = 0;
-    'running: loop {
+
+    loop {
         i = (&i + 1) % 255;
         canvas.set_draw_color(Color::RGB(i, 64, 255 - &i));
         canvas.clear();
@@ -49,16 +45,34 @@ pub fn main() {
 
         for event in event_pump.poll_iter() {
             match event {
-                Event::Quit {..} |
+                Event::Quit { .. } |
                 Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
-                    break 'running
+                    return
                 },
                 _ => {}
             }
         }
-        // The rest of the game loop goes here...
 
         canvas.present();
         ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
     }
+}
+
+pub fn main() {
+    let sdl_context = sdl2::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
+
+    let window = video_subsystem.window("Rusty snake", MAP_X * MAP_TILE_SIZE, MAP_Y * MAP_TILE_SIZE)
+        .position_centered()
+        .build()
+        .unwrap();
+
+    let mut canvas = window.into_canvas().build().unwrap();
+    canvas.set_draw_color(Color::RGB(0, 255, 255));
+    canvas.clear();
+    canvas.present();
+
+    let map = init_map();
+
+    game_loop(sdl_context, canvas, map);
 }
