fb7f3284ea24b853977e308812f9a09cd6abb35f
Octave Larose <octave.larose@hotmail.fr>
2020-12-31 12:35:59 +0100

    More function splitting

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -6,6 +6,10 @@ use sdl2::keyboard::Keycode;
 use std::time::Duration;
 use sdl2::rect::Rect;
 
+use crate::UpdateValue::GameStop;
+
+enum UpdateValue { Ok, GameStop }
+
 const MAP_X: u32 = 50;
 const MAP_Y: u32 = 50;
 const MAP_SIZE: u32 = MAP_X * MAP_Y;
@@ -24,36 +28,57 @@ fn init_map() -> [Rect; MAP_SIZE as usize] {
     map
 }
 
-fn game_loop(sdl_context: sdl2::Sdl,
-             mut canvas: sdl2::render::Canvas<sdl2::video::Window>,
-             map: [Rect; MAP_SIZE as usize]) {
+fn update(event_pump: &mut sdl2::EventPump, i: &mut u8) -> UpdateValue {
+    *i = (*i + 1) % 255;
+
+    for event in event_pump.poll_iter() {
+        match event {
+            Event::Quit { .. } |
+            Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
+                return GameStop;
+            },
+            Event::KeyDown { keycode: Some(Keycode::Up), .. } |
+            Event::KeyDown { keycode: Some(Keycode::Down), .. } |
+            Event::KeyDown { keycode: Some(Keycode::Left), .. } |
+            Event::KeyDown { keycode: Some(Keycode::Right), .. }  => {
+                print!("Key pressed\n");
+                println!("{:?}", event);
+            },
+            _ => {}
+        }
+    }
+
+    UpdateValue::Ok
+}
+
+fn render(canvas: &mut sdl2::render::Canvas<sdl2::video::Window>,
+          map: &[Rect; MAP_SIZE as usize],
+          i: u8) {
+    canvas.set_draw_color(Color::RGB(i, 64, 255 - &i));
+    canvas.clear();
+
+    canvas.set_draw_color(Color::RGB(0, 0, 0));
+    for i in 0..MAP_SIZE {
+        let res: std::result::Result<_, _> = canvas.draw_rect(map[i as usize]);
+        if res.is_err() {
+            print!("Error when drawing rectangle {}", i);
+        }
+    }
+
+    canvas.present();
+}
+
+fn game_loop(sdl_context: &sdl2::Sdl,
+             canvas: &mut sdl2::render::Canvas<sdl2::video::Window>,
+             map: &[Rect; MAP_SIZE as usize]) {
     let mut event_pump = sdl_context.event_pump().unwrap();
     let mut i = 0;
 
     loop {
-        i = (&i + 1) % 255;
-        canvas.set_draw_color(Color::RGB(i, 64, 255 - &i));
-        canvas.clear();
-
-        canvas.set_draw_color(Color::RGB(0, 0, 0));
-        for i in 0..MAP_SIZE {
-            let res: std::result::Result<_, _> = canvas.draw_rect(map[i as usize]);
-            if res.is_err() {
-                print!("Error when drawing rectangle {}", i);
-            }
+        if let UpdateValue::GameStop = update(&mut event_pump, &mut i) {
+            return
         }
-
-        for event in event_pump.poll_iter() {
-            match event {
-                Event::Quit { .. } |
-                Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
-                    return
-                },
-                _ => {}
-            }
-        }
-
-        canvas.present();
+        render(canvas, &map, i);
         ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
     }
 }
@@ -74,5 +99,5 @@ pub fn main() {
 
     let map = init_map();
 
-    game_loop(sdl_context, canvas, map);
+    game_loop(&sdl_context, &mut canvas, &map);
 }
